ad4fb313666aca49c9885958c459ceeb7879670a
ISHRAQ01 <ishraq0641@gmail.com>
2026-05-16 15:43:33 +0530

    FLEET MANAGEMENT PROJECT ADDED

---
+++ FleetManagement-Backend/src/test/java/com/project/FleetManagement/FleetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.FleetManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
