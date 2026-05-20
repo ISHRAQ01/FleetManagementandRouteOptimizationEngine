0295f305a8bef62383438e4eecf817c18f7000f9
ISHRAQ01 <ishraq0641@gmail.com>
2026-05-20 19:06:48 +0530

    day 4: DeliveryTask logics updated

--- FleetManagement-Backend/src/main/java/com/project/FleetManagement/entity/DeliveryTask.java
+++ FleetManagement-Backend/src/main/java/com/project/FleetManagement/entity/DeliveryTask.java
@@ -1,4 +1,55 @@
 package com.project.FleetManagement.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="DELIVERY_TASK")
+@Data
+@NoArgsConstructor
 public class DeliveryTask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderNumber;
+    private String pickupAdd;
+    private String deliveryAdd;
+    private String status;
+    private Double pickupLat;
+    private Double pickupLng;
+    private Double deliveryLat;
+    private Double deliveryLng;
+
+    @ManyToOne
+    @JoinColumn(name="vehicle_id")
+    private Vehicle vehicle;
+
+    @ManyToOne
+    @JoinColumn(name="driver_id")
+    private Driver driver;
+
+    private LocalDateTime statusUpdatedAt;
+
+    public DeliveryTask(String orderNumber, String pickupAdd, String deliveryAdd, String status,Double pickupLat,Double pickupLng,Double deliveryLat,Double deliveryLng) {
+        this.orderNumber = orderNumber;
+        this.pickupAdd = pickupAdd;
+        this.deliveryAdd = deliveryAdd;
+        this.status = status;
+        this.pickupLat = pickupLat;
+        this.pickupLng = pickupLng;
+        this.deliveryLat = deliveryLat;
+        this.deliveryLng = deliveryLng;
+        this.statusUpdatedAt = LocalDateTime.now();
+
+    }
+
+
+
 }
--- FleetManagement-Backend/src/main/java/com/project/FleetManagement/repository/DeliveryTaskRepository.java
+++ FleetManagement-Backend/src/main/java/com/project/FleetManagement/repository/DeliveryTaskRepository.java
@@ -1,4 +1,24 @@
 package com.project.FleetManagement.repository;
 
-public class DeliveryTaskRepository {
+import com.project.FleetManagement.entity.Driver;
+import com.project.FleetManagement.entity.Vehicle;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.project.FleetManagement.entity.DeliveryTask;
+import java.util.List;
+
+@Repository
+public interface DeliveryTaskRepository extends JpaRepository<DeliveryTask,Long> {
+    //Find all task
+    List<DeliveryTask>findByStatus(String status);
+
+    //find task assigned to specific vehicle
+    List<DeliveryTask>findByVehicle(Vehicle vehicle);
+
+    //find task assigned to specific driver
+    List<DeliveryTask>findByDriver(Driver driver);
+
+    //find task taken by staus and driver
+    List<DeliveryTask>findByStatusAndDriver(String status,Driver driver);
+
 }
