580ba1f2dd685c3e020abcada4f4dac5fd84718a
ISHRAQ01 <ishraq0641@gmail.com>
2026-05-20 19:06:27 +0530

    day 4: Driver logics updated

--- FleetManagement-Backend/src/main/java/com/project/FleetManagement/entity/Driver.java
+++ FleetManagement-Backend/src/main/java/com/project/FleetManagement/entity/Driver.java
@@ -2,32 +2,34 @@ package com.project.FleetManagement.entity;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
 @Table(name="DRIVER")
 public class Driver {
    @Id
    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
-   @Column(nullable = false,unique = true)
-    private long licenseNumber;
-
-   @Column(nullable = false)
     private String name;
+    private String licenseNumber;
+    private String shiftStart;
+    private String shiftEnd;
+    private String phone;
+    private String status;
 
-   @Column(nullable = false)
-    private String phoneNumber;
-
-    public Driver() {}
-
-    public Driver(long licenseNumber, String name, String phoneNumber) {
-        this.licenseNumber = licenseNumber;
+    public Driver(String name, String licenseNumber, String shiftStart, String shiftEnd, String phone, String status) {
         this.name = name;
-        this.phoneNumber = phoneNumber;
-
+        this.licenseNumber = licenseNumber;
+        this.shiftStart = shiftStart;
+        this.shiftEnd = shiftEnd;
+        this.phone = phone;
+        this.status = status;
     }
+
 
 }
--- FleetManagement-Backend/src/main/java/com/project/FleetManagement/repository/DriverRepository.java
+++ FleetManagement-Backend/src/main/java/com/project/FleetManagement/repository/DriverRepository.java
@@ -1,4 +1,17 @@
 package com.project.FleetManagement.repository;
 
-public class DriverRepository {
+import com.project.FleetManagement.entity.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DriverRepository extends JpaRepository<Driver, Long> {
+    //query to find driver by status
+    List<Driver>findByStatus(String status);
+
+    //query to find driver by license number
+    List<Driver>findByLicenseNumber(String licenseNumber);
+
 }
