9145fb3ce8012b29799dcc79d86f13b44aba3918
ISHRAQ01 <ishraq0641@gmail.com>
2026-05-21 14:52:55 +0530

    DAY 5: DeliveryTaskController SERVICE LOGIC ADDED

--- FleetManagement-Backend/src/main/java/com/project/FleetManagement/controller/DeliveryTaskController.java
+++ FleetManagement-Backend/src/main/java/com/project/FleetManagement/controller/DeliveryTaskController.java
@@ -1,4 +1,60 @@
 package com.project.FleetManagement.controller;
 
+import com.project.FleetManagement.service.DeliveryTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import com.project.FleetManagement.entity.*;
+
+@RestController
+@RequestMapping("api/delivery-tasks")
 public class DeliveryTaskController {
+
+    @Autowired
+    private DeliveryTaskService deliveryTaskService;
+
+    //Create a task
+    @PostMapping
+    public ResponseEntity<DeliveryTask> createTask(@RequestBody DeliveryTask task){
+        return new ResponseEntity<>(deliveryTaskService.createDeliveryTask(task), HttpStatus.CREATED);
+    }
+
+    //Read all task
+    @GetMapping
+    public ResponseEntity<List<DeliveryTask>> getAllTask(){
+        return ResponseEntity.ok(deliveryTaskService.getAllTasks());
+    }
+
+    //read task by id
+    @GetMapping("/{id}")
+    public ResponseEntity<DeliveryTask> getTaskById(@PathVariable Long id){
+        return ResponseEntity.ok(deliveryTaskService.getTaskById(id));
+    }
+
+    @PutMapping("/{taskId}/assign")
+    public ResponseEntity<DeliveryTask> assignTask(@PathVariable Long taskId,@RequestParam Long vehicleId,@RequestParam Long driverId){
+        DeliveryTask assignedTask=deliveryTaskService.assignTask(taskId,vehicleId,driverId);
+        return ResponseEntity.ok(assignedTask);
+    }
+
+    @PatchMapping("/{taskId}/status")
+    public ResponseEntity<DeliveryTask> updateTaskStatus(@PathVariable Long taskId,@RequestParam String newStatus){
+        DeliveryTask deliveryTask=deliveryTaskService.updateStatus(taskId,newStatus);
+        return ResponseEntity.ok(deliveryTask);
+    }
+
+    @GetMapping("/status/{status}")
+    public ResponseEntity<List<DeliveryTask>> getTaskByStatus(@PathVariable String status){
+        return ResponseEntity.ok(deliveryTaskService.getTaskByStatus(status));
+    }
+
+    //Delete Task
+    @DeleteMapping("/{id}")
+    public ResponseEntity<DeliveryTask> deleteTask(@PathVariable Long id){
+        deliveryTaskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- FleetManagement-Backend/src/main/java/com/project/FleetManagement/service/DeliveryTaskService.java
+++ FleetManagement-Backend/src/main/java/com/project/FleetManagement/service/DeliveryTaskService.java
@@ -1,4 +1,77 @@
 package com.project.FleetManagement.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.project.FleetManagement.repository.*;
+import com.project.FleetManagement.entity.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
 public class DeliveryTaskService {
+    @Autowired
+    private DeliveryTaskRepository deliveryTaskRepository;
+
+    @Autowired
+    private DriverRepository driverRepository;
+
+    @Autowired
+    private VehicleRepository vehicleRepository;
+
+    // Create new delivery task
+    public DeliveryTask createDeliveryTask(DeliveryTask task) {
+        task.setStatus("UNASSIGNED");
+        task.setStatusUpdatedAt(LocalDateTime.now());
+        return deliveryTaskRepository.save(task);
+    }
+
+    //Get all tasks
+    public List<DeliveryTask> getAllTasks() {
+        return deliveryTaskRepository.findAll();
+    }
+
+    // Get task by ID
+    public DeliveryTask getTaskById(Long id) {
+        return deliveryTaskRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Task not found with id: " + id));
+    }
+
+    //Assign vehicle and driver to a task
+    public DeliveryTask assignTask(Long taskId, Long vehicleId, Long driverId) {
+        DeliveryTask task = getTaskById(taskId);
+        Vehicle vehicle = vehicleRepository.findById(vehicleId).orElseThrow(() -> new RuntimeException("Vehicle not found with id: " + vehicleId));
+        Driver driver = driverRepository.findById(driverId).orElseThrow(() -> new RuntimeException("Driver not found with id: " + driverId));
+
+        task.setVehicle(vehicle);
+        task.setDriver(driver);
+        task.setStatus("ASSIGNED");
+        task.setStatusUpdatedAt(LocalDateTime.now());
+
+        return deliveryTaskRepository.save(task);
+    }
+
+    //update task status(UNASSIGNED -> DISPATCHED -> IN_TRANSIT -> DELIVERED)
+
+    public DeliveryTask updateStatus(Long taskId,String newStatus){
+        DeliveryTask task=getTaskById(taskId);
+        task.setStatus(newStatus);
+        task.setStatusUpdatedAt(LocalDateTime.now());
+
+        return deliveryTaskRepository.save(task);
+    }
+
+    //Get task by status
+    public List<DeliveryTask>getTaskByStatus(String status){
+
+        return deliveryTaskRepository.findByStatus(status);
+    }
+
+    //Delete task
+    public void deleteTask(Long taskId){
+
+        deliveryTaskRepository.deleteById(taskId);
+    }
+
 }
+
+
